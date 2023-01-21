603e00a632966fae762b1f69c41630d3235ea3c2
Vincent Hagen <vinnie@script4web.nl>
2023-01-21 13:20:36 +0100

    Add correct crossOrigin attr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
                 <link
                     rel="preconnect"
                     href="https://fonts.gstatic.com"
-                    crossOrigin="true"
+                    crossOrigin="anonymous"
                 />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Oxygen&family=Roboto:wght@300&display=swap"
