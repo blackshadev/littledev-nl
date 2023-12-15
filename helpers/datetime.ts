85c12b4ce2aabf48817e71c439047c0ea858daf0
Vincent Hagen <vinnie@script4web.nl>
2023-12-15 23:17:53 +0100

    remove debug things as it doesn't help. set locale

--- helpers/datetime.ts
+++ helpers/datetime.ts
@@ -2,6 +2,8 @@ import dayjs from 'dayjs';
 
 const DISPLAY_FORMAT = 'DD/MM/YYYY';
 
+dayjs.locale('en');
+
 export function formatAsDate(date: string | Date): string {
     return dayjs(date).format(DISPLAY_FORMAT);
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
