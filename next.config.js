06c44556f2ae3d334dc1f7b72223e84db3f7ceeb
Vincent Hagen <vinnie@script4web.nl>
2023-12-15 22:53:20 +0100

    Add production source maps, move to bun

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
